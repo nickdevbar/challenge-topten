33bbfe121771607caba45f6fcc2cf4de12df463d
LuisUcla <caluigi7@gmail.com>
2023-12-05 08:59:55 -0500

    refactor: add aditional config

--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 from decouple import config
 
-class Config():
+class Config(object):
     SECRET_KEY = config('SECRET_KEY')
-    print(SECRET_KEY)
+    TESTING = False
+    # DB_SERVER = '192.168.1.56'
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+# class ProductionConfig(Config):
+#     """Uses production database server."""
+#     DB_SERVER = '192.168.19.32'
+
+# class TestingConfig(Config):
+#     DB_SERVER = 'localhost'
+#     DATABASE_URI = 'sqlite:///:memory:'
 
 
 config = {
